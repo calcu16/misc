13f1804850406c1fec7f412bb71affa7e8e4d300
Andrew Carter <acarter@linkedin.com>
2014-09-10 10:08:58 -0700

    Grabbing a hacked up script to hack up for demos

---
+++ demo.c
@@ -0,0 +1,381 @@
+/*
+ * Copyright (c) 1980, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifdef __linux__
+#define _BSD_SOURCE 1
+#define _POSIX_C_SOURCE 3
+#endif
+
+#ifndef __FBSDID
+#define __FBSDID(x)  static const char freebsd_id[] = { x }
+#endif
+
+#include <sys/cdefs.h>
+
+__FBSDID("$FreeBSD: src/usr.bin/script/script.c,v 1.24 2004/02/15 17:30:13 cperciva Exp $");
+
+#ifndef lint
+static const char copyright[] =
+"@(#) Copyright (c) 1980, 1992, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
+#endif
+
+#ifndef lint
+static const char sccsid[] = "@(#)script.c	8.1 (Berkeley) 6/6/93";
+#endif
+
+#include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#if !defined(__APPLE__) && !defined(__linux__)
+#include <libutil.h>
+#endif /* !__APPLE__ */
+#include <paths.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+#ifdef __APPLE__
+#include <util.h>
+#endif /* __APPLE__ */
+#ifdef __linux__
+#include <pty.h>
+#include <utmp.h>
+#endif
+
+#ifndef __dead2
+#define __dead2		__attribute__((__noreturn__))
+#endif
+
+FILE	*fscript;
+int	master, slave;
+int	child;
+const char *fname;
+int	qflg, ttyflg;
+struct timeval period;
+struct	termios tt;
+
+void	done(int) __dead2;
+void	dooutput(void);
+void	doshell(char **);
+void	fail(void);
+void	finish(void);
+void	usage(void);
+void	writetosubprocess(const char*, int);
+void	writetouser(const char*, int);
+void	writetolog(const char*, int);
+void	periodic(void);
+bool	isechoing(void);
+void	setperiod(double);
+
+int
+main(int argc, char *argv[])
+{
+	int cc;
+	struct termios rtt;
+	struct winsize win;
+	int aflg, kflg, ch, n;
+	struct timeval start, elapsed, timeout, *timeoutp;
+	time_t tvec;
+	char obuf[BUFSIZ];
+	char ibuf[BUFSIZ];
+	fd_set rfd;
+	setperiod(30.0);
+
+	aflg = kflg = 0;
+	while ((ch = getopt(argc, argv, "aqkt:")) != -1) {
+		switch(ch) {
+		case 'a':
+			aflg = 1;
+			break;
+		case 'q':
+			qflg = 1;
+			break;
+		case 'k':
+			kflg = 1;
+			break;
+		case 't':
+			setperiod(atof(optarg));
+			break;
+		case '?':
+		default:
+			usage();
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (argc > 0) {
+		fname = argv[0];
+		argv++;
+		argc--;
+	} else
+		fname = "typescript";
+
+	if ((fscript = fopen(fname, aflg ? "a" : "w")) == NULL)
+		err(1, "%s", fname);
+
+	if ((ttyflg = isatty(STDIN_FILENO))) {
+		if (tcgetattr(STDIN_FILENO, &tt) == -1)
+			err(1, "tcgetattr");
+		if (ioctl(STDIN_FILENO, TIOCGWINSZ, &win) == -1)
+			err(1, "ioctl");
+		if (openpty(&master, &slave, NULL, &tt, &win) == -1)
+			err(1, "openpty");
+	} else {
+		if (openpty(&master, &slave, NULL, NULL, NULL) == -1)
+			err(1, "openpty");
+	}
+
+	if (!qflg) {
+		tvec = time(NULL);
+		(void)printf("Script started, output file is %s\n", fname);
+		(void)fprintf(fscript, "Script started on %s", ctime(&tvec));
+		fflush(fscript);
+	}
+	if (ttyflg) {
+		rtt = tt;
+		cfmakeraw(&rtt);
+		rtt.c_lflag &= ~ECHO;
+		(void)tcsetattr(STDIN_FILENO, TCSAFLUSH, &rtt);
+	}
+
+	child = fork();
+	if (child < 0) {
+		warn("fork");
+		done(1);
+	}
+	if (child == 0)
+		doshell(argv);
+
+	(void)close(slave);
+
+
+	timeout = period;
+	gettimeofday(&start, NULL);
+	FD_ZERO(&rfd);
+	for (;;) {
+		FD_SET(master, &rfd);
+		FD_SET(STDIN_FILENO, &rfd);
+		if (timeout.tv_sec || timeout.tv_usec)
+			timeoutp = &timeout;
+		else
+			timeoutp = NULL;
+		n = select(master + 1, &rfd, 0, 0, timeoutp);
+		if (n < 0 && errno != EINTR)
+			break;
+		if (n > 0 && FD_ISSET(STDIN_FILENO, &rfd)) {
+			cc = read(STDIN_FILENO, ibuf, BUFSIZ);
+			writetosubprocess(ibuf, cc);
+			if (kflg && !isechoing()) {
+				writetolog(ibuf, cc);
+			}
+		}
+		if (n > 0 && FD_ISSET(master, &rfd)) {
+			cc = read(master, obuf, sizeof (obuf));
+			if (cc <= 0)
+				break;
+			writetouser(obuf, cc);
+			writetolog(obuf, cc);
+		}
+		gettimeofday(&elapsed, NULL);
+		timersub(&elapsed, &start, &elapsed);
+		if (timercmp(&elapsed, &timeout, >)) {
+			periodic();
+			timeout = period;
+		} else {
+			timersub(&timeout, &elapsed, &timeout);
+			if (timercmp(&timeout, &period, >))
+				timeout = period;
+		}
+		gettimeofday(&start, NULL);
+	}
+	finish();
+	done(0);
+}
+
+
+void setperiod(double intervaltime)
+{
+	if (intervaltime < 0)
+		err(1, "invalid time interval %f", intervaltime);
+	double intpart, fracpart;
+	fracpart = modf(intervaltime, &intpart);
+	period.tv_sec = (int) intpart;
+	period.tv_usec = (int) (fracpart * 1000000.0);
+}
+
+
+void
+writetosubprocess(const char* buf, int cc)
+{
+	if (cc <= 0)
+	       return;
+	(void)write(master, buf, cc);
+}
+
+
+void
+writetouser(const char* buf, int cc)
+{
+	if (cc <= 0)
+	       return;
+	(void)write(STDOUT_FILENO, buf, cc);
+}
+
+
+void
+writetolog(const char* buf, int cc)
+{
+	(void)fwrite(buf, 1, cc, fscript);
+}
+
+
+void
+flushlog(void)
+{
+	fflush(fscript);
+}
+
+
+bool
+isechoing()
+{
+	struct termios stt;
+	return tcgetattr(master, &stt) >= 0 && ((stt.c_lflag & ECHO) != 0);
+}
+
+
+void
+periodic(void)
+{
+	flushlog();
+}
+
+
+void
+usage(void)
+{
+	(void)fprintf(stderr,
+	    "usage: script [-akq] [-t time] [file [command ...]]\n");
+	exit(1);
+}
+
+
+void
+finish(void)
+{
+	pid_t pid;
+	int die, e, status;
+
+	die = e = 0;
+	while ((pid = wait3(&status, WNOHANG, 0)) > 0)
+		if (pid == child) {
+			die = 1;
+			if (WIFEXITED(status))
+				e = WEXITSTATUS(status);
+			else if (WIFSIGNALED(status))
+				e = WTERMSIG(status);
+			else /* can't happen */
+				e = 1;
+		}
+
+	if (die)
+		done(e);
+}
+
+
+void
+doshell(char **av)
+{
+	const char *shell;
+
+	shell = getenv("SHELL");
+	if (shell == NULL)
+		shell = _PATH_BSHELL;
+
+	(void)close(master);
+	(void)fclose(fscript);
+	login_tty(slave);
+	if (av[0]) {
+		execvp(av[0], av);
+		warn("%s", av[0]);
+	} else {
+		execl(shell, shell, "-i", (char *)NULL);
+		warn("%s", shell);
+	}
+	fail();
+}
+
+
+void
+fail(void)
+{
+	(void)kill(0, SIGTERM);
+	done(1);
+}
+
+
+void
+done(int eno)
+{
+	time_t tvec;
+
+	if (ttyflg)
+		(void)tcsetattr(STDIN_FILENO, TCSAFLUSH, &tt);
+	if (fscript) {
+		tvec = time(NULL);
+		if (!qflg) {
+			(void)fprintf(fscript,
+					"\nScript done on %s", ctime(&tvec));
+			(void)printf("\nScript done, output file is %s\n",
+					fname);
+		}
+		(void)fclose(fscript);
+	}
+	if (master)
+		(void)close(master);
+	exit(eno);
+}
