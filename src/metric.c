357b4988dc05a99cb10577a0cbe8d6618cdad2d1
Andrew Carter <acarter@linkedin.com>
2015-08-28 10:06:24 -0700

    Adding a protocol

--- src/metric.c
+++ src/metric.c
@@ -6,7 +6,7 @@
 #include <sys/wait.h>
 
 #define LENGTHOF(a) (sizeof(a)/sizeof(*(a)))
-static const char usage[] = "usage: %s PROG_NAME SAMPLE FABRIC HOST CLIENT_SERVICE DIRECTION SERVER_SERVICE\n";
+static const char usage[] = "usage: %s PROG_NAME SAMPLE FABRIC HOST PROTOCOL CLIENT_SERVICE DIRECTION SERVER_SERVICE\n";
 static const char fmt[] = "%*lu client: seq %*lu: %ld %*lu %*lu %ld %ld %*lu %*lu %ld +/- %ld %ld";
 
 int cmp(const void *ap, const void *bp) {
@@ -30,6 +30,7 @@ enum {
   SAMPLE,
   FABRIC,
   CLIENT_HOST,
+  PROTOCOL,
   CLIENT_SERVICE,
   DIRECTION,
   SERVER_SERVICE,
@@ -48,7 +49,7 @@ int main(int argc, char** argv) {
   int64_t out_start, out_end, in_start, in_end, lower_delta, upper_delta, i;
   char * submit_prog = argv[SUBMIT_PROG];
   char *fabric = argv[FABRIC], *client_host = argv[CLIENT_HOST], *client_service = argv[CLIENT_SERVICE];
-  char *direction = argv[DIRECTION], *server_service = argv[SERVER_SERVICE];
+  char *protocol = argv[PROTOCOL], *direction = argv[DIRECTION], *server_service = argv[SERVER_SERVICE];
   char metric_buffer[256], value_buffer[256];
   int n, index[LENGTHOF(PERCENTILES)];
   double sum;
@@ -74,11 +75,11 @@ int main(int argc, char** argv) {
     qsort(in_buffer, sample, sizeof(int64_t), cmp);
 
     for (i = 0; i < LENGTHOF(index); ++i) {
-      sprintf(metric_buffer, "client_%s_%s_%s_lat%dth", client_service, direction, server_service, PERCENTILES[i]);
+      sprintf(metric_buffer, "client_%s_%s_%s_%s_lat%dth", client_service, protocol, direction, server_service, PERCENTILES[i]);
       sprintf(value_buffer, "%ld", out_buffer[index[i]]);
       submit(submit_prog, fabric, metric_buffer, client_host, value_buffer);
 
-      sprintf(metric_buffer, "server_%s_%s_%s_lat%dth", server_service, direction, client_service, PERCENTILES[i]);
+      sprintf(metric_buffer, "server_%s_%s_%s_%s_lat%dth", server_service, protocol, direction, client_service, PERCENTILES[i]);
       sprintf(value_buffer, "%ld", in_buffer[index[i]]);
       submit(submit_prog, fabric, metric_buffer, client_host, value_buffer);
     }
@@ -89,11 +90,11 @@ int main(int argc, char** argv) {
         max = out_buffer[i];
       }
     }
-    sprintf(metric_buffer, "client_%s_%s_%s_latmax", client_service, direction, server_service);
+    sprintf(metric_buffer, "client_%s_%s_%s_%s_latmax", protocol, client_service, direction, server_service);
     sprintf(value_buffer, "%ld", max);
     submit(submit_prog, fabric, metric_buffer, client_host, value_buffer);
 
-    sprintf(metric_buffer, "client_%s_%s_%s_latavg", client_service, direction, server_service);
+    sprintf(metric_buffer, "client_%s_%s_%s_%s_latavg", protocol, client_service, direction, server_service);
     sprintf(value_buffer, "%f", sum / sample);
     submit(submit_prog, fabric, metric_buffer, client_host, value_buffer);
 
@@ -103,11 +104,11 @@ int main(int argc, char** argv) {
         max = in_buffer[i];
       }
     }
-    sprintf(metric_buffer, "server_%s_%s_%s_latmax", server_service, direction, client_service);
+    sprintf(metric_buffer, "server_%s_%s_%s_%s_latmax", protocol, server_service, direction, client_service);
     sprintf(value_buffer, "%ld", max);
     submit(submit_prog, fabric, metric_buffer, client_host, value_buffer);
 
-    sprintf(metric_buffer, "server_%s_%s_%s_latavg", server_service, direction, client_service);
+    sprintf(metric_buffer, "server_%s_%s_%s_%s_latavg", protocol, server_service, direction, client_service);
     sprintf(value_buffer, "%f", sum / sample);
     submit(submit_prog, fabric, metric_buffer, client_host, value_buffer);
   }
